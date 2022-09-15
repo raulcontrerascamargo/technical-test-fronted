310bde94f8041d23aa9edfac6e0d7d4633415dc4
rocanrul <rockandrul120@gmail.com>
2022-09-15 13:48:52 -0500

    Start

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NewEmployeComponent } from './components/new-employe/new-employe.component';
+import { ListComponent } from './components/list/list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    NewEmployeComponent,
+    ListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
